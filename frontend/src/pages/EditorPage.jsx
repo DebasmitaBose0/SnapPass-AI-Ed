72b6dfd8439ac2f7c8be85c53559f492a3d80478
Babin123456 <babindoraswamy@gmail.com>
2026-08-04 11:38:57 +0530

    feat: implement interactive snap pass ai photo assistant chatbot and knowledge base

--- frontend/src/pages/EditorPage.jsx
+++ frontend/src/pages/EditorPage.jsx
@@ -16,6 +16,7 @@ import { cachePhotoOffline } from '../services/indexedDb';
 import api from '../services/api';
 import { autoEnhanceImage } from '../utils/imageEnhancer';
 import { AttireManualAdjuster } from '../components/AttireManualAdjuster';
+import PassportAssistantChatbot from '../chatbot/PassportAssistantChatbot';
 import { uploadPhoto } from '../services/photoService';
 import './EditorPage.css';
 
@@ -574,6 +575,7 @@ function EditorPage({ darkMode, toggleTheme }) {
           </motion.div>
         </div>
       </div>
+      <PassportAssistantChatbot />
     </div>
   );
 }
