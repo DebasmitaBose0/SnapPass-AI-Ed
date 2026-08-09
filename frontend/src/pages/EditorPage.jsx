12f61c13464421107c2beaa9bb24445f724132e0
Babin123456 <babin123456@users.noreply.github.com>
2026-08-09 19:41:05 +0530

    feat: add real-time photo RGB histogram graph and exposure inspector

--- frontend/src/pages/EditorPage.jsx
+++ frontend/src/pages/EditorPage.jsx
@@ -20,7 +20,7 @@ import { cachePhotoOffline } from '../services/indexedDb';
 import api from '../services/api';
 import { autoEnhanceImage } from '../utils/imageEnhancer';
 import { AttireManualAdjuster } from '../components/AttireManualAdjuster';
-import PassportAssistantChatbot from '../chatbot/PassportAssistantChatbot';
+import { PhotoHistogramInspector } from '../components/editor/PhotoHistogramInspector';
 import { uploadPhoto } from '../services/photoService';
 import './EditorPage.css';
 
